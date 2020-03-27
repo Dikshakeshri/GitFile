b839ff88e465794010cf0f4c5a21bd983393f2e5
Dikshakeshri <dikshakeshri12@gmail.com>
2020-03-27 15:58:55 +0530

    this is 1st commit

---
+++ GreetGit/src/com/ntt/simple.java
@@ -0,0 +1,18 @@
+package com.ntt;
+
+import java.util.Scanner;
+
+public class simple {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter name");
+		String n=sc.nextLine();
+		System.out.println("welocme to github :" +n);
+		String s1="  hello   ";  
+		System.out.println(s1+"how are you");  
+				// TODO Auto-generated method stub
+
+	}
+
+}
